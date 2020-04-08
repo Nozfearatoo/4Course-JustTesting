a9b4d4eca1e8914c63fde18d77d609644ed9608b
Nozfearatoo <vs.vasilevskiy@gmail.com>
2020-04-09 00:48:01 +0300

    Initial commit

---
+++ src/MainClass.java
@@ -0,0 +1,20 @@
+public class MainClass {
+    public static void main(String[] args)
+    {
+        System.out.print ("Hello, world! \n");
+        System.out.println("Bye, world!");
+        int x=5;
+        int y=6;
+        System.out.printf("x=%d; y=%d \n", x, y);
+        String name = "Tom";
+        int age = 30;
+        float height = 1.7f;
+        System.out.printf("Name: %s  Age: %d  Height: %.3f \n", name, age, height);
+    }
+}
+
+//комментарий раз
+
+/*
+коммент номер два
+ */
